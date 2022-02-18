4afdfd700b87a22850adcfca9121a8085b79114f
Danilo Arruda <da.ni.lo_arruda@outlook.com>
2022-02-18 19:48:22 -0300

    remove code

--- src/main/java/barbershop/clientmodule/domain/service/exception/BarberNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package barbershop.clientmodule.domain.service.exception;
-
-import java.util.UUID;
-
-public class BarberNotFoundException extends Exception {
-    public BarberNotFoundException(UUID barberId) {
-        super("barber with id " + barberId + " not found");
-    }
-}
--- src/main/java/barbershop/clientmodule/domain/service/exception/TaskNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package barbershop.clientmodule.domain.service.exception;
-
-import java.util.UUID;
-
-public class TaskNotFoundException extends Exception {
-    public TaskNotFoundException(UUID taskId) {
-        super("task with id " + taskId + " not found");
-    }
-}
